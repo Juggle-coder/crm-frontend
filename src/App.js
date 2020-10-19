c97a026434c3c8858ef268adc57974c6be740caf
Juggle-coder <sujansharma3383@gmail.com>
2020-10-19 20:54:33 +1100

    layout is added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-
 import './App.css';
+import { DefaultLayout } from './layout/DefaultLayout';
 
 // import { Button } from 'react-bootstrap';//
-import { Entry } from './pages/entry/Entry.page';
+// import { Entry } from './pages/entry/Entry.page';
 
 
 
 function App() {
   return (
     <div className="App">
-     <Entry />
+     {/* <Entry /> */}
+     <DefaultLayout>
+       Dashboard
+      </DefaultLayout>
     </div>
   );
 }
