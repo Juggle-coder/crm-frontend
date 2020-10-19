ac4be286ba8f2b6065f097fe506b8c1deaeffabd
Juggle-coder <sujansharma3383@gmail.com>
2020-10-19 22:46:30 +1100

    Dashboard added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { DefaultLayout } from './layout/DefaultLayout';
+import { Dashboard } from './pages/dashboard/Dashboard.page';
+
 
 // import { Button } from 'react-bootstrap';//
 // import { Entry } from './pages/entry/Entry.page';
@@ -12,7 +14,7 @@ function App() {
     <div className="App">
      {/* <Entry /> */}
      <DefaultLayout>
-       Dashboard
+       <Dashboard/>
       </DefaultLayout>
     </div>
   );
